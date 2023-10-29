d9dbbb96c3f05f3a732e180d099a0b75973f3efe
SebasTangQ <sebastian.tang@usm.cl>
2023-10-29 17:29:23 -0300

    LittleChange

--- ProyectoEcoSansanoJMC/EcoSansanoJMC/webEco/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebecoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webEco'
--- ProyectoEcoSansanoJMC/EcoSansanoJMC/webEco/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('compostaje/', views.compostaje, name='compostaje'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('recoleccion/', views.recoleccion, name='recoleccion'),
-]
